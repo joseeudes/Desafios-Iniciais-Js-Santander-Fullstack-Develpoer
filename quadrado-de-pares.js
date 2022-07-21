ed9726680da23ff5b900d228655aacc052f69d8f
José Eudes <105015999+joseeudes@users.noreply.github.com>
2022-07-21 08:28:37 -0300

    Add files via upload

---
+++ quadrado-de-pares.js
@@ -0,0 +1,13 @@
+// a função gets() é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print() para imprimir a saída (output) de dados e já pula uma linha ("\n")
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+let lines = gets().split('\n');
+var N = parseInt(lines.shift());
+
+for (let i = 1; i <= N; i++) {
+let quadrado = Math.pow(i, 2);
+
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+
+	if (i % 2 === 0) 
+          print(`${i}^2 = ${quadrado}`);
+}
